bad1141b94c08acca4b27897ec40de46bed12047
jampttws <tanasorn42@hotmail.com>
2019-04-25 17:53:12 +0700

    updated

--- myapp/app.js
+++ myapp/app.js
@@ -2,10 +2,12 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var gameRouter = require('./routes/game')
 
 var app = express();
 
@@ -14,12 +16,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/game', gameRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
